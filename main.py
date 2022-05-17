fc48c247acbef26d000272fd5c9295f348101926
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-05-17 13:12:40 +0700

    add count of loop iterations

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+
 color_wn = input('Color background: ')
 wn = turtle.Screen()
 wn.bgcolor(color_wn)
@@ -10,12 +11,13 @@ tess.color(color_tess)
 
 distance = 10
 angle = 36
-#tess.penup
-for t in range(30):
+# tess.penup
+count_loop = int(input('Count of loop iterations: '))
+for t in range(count_loop):
     tess.stamp()
     tess.forward(distance)
     tess.left(angle)
     distance = distance + 5
-    #angle = angle - 3
+    # angle = angle - 3
 
 wn.exitonclick()
