d3ca2b24d6ea09cb853e0847f3dad155e2430973
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-05-19 00:25:59 +0700

    euclidean_init

--- week2/euclidean_distance.py
+++ week2/euclidean_distance.py
@@ -3,12 +3,11 @@ b = [77, 23, 43, 56, 63, 9, 32, 5, 21, 34]
 c = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 arr_sum = 0
 
-print(len(a) == len(b))
+print('Equal lengths of vectors: ', len(a) == len(b))
 
 for i in range(len(a)):
-    c[i] = (b[i] - a[i])**2
+    c[i] = (b[i] - a[i]) ** 2
     arr_sum = arr_sum + c[i]
-print(c)
-print(arr_sum)
+
 distance = arr_sum ** 0.5
-print(distance)
+print('Euclidean distance between vectors:', round(distance, 3))
