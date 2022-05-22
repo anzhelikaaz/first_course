eff0f104f4d9c56659afe7762413e96e19ae83bd
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-05-22 15:20:52 +0700

    fast checking

--- week2/first_part_str.py
+++ week2/first_part_str.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 def first_part(x):
     return x[:len(x) // 2]
 
@@ -21,3 +23,29 @@ print()
 
 for ch in s[:len(s) // 2]:
     print(ch, end='')
+
+t0 = datetime.now()
+for _ in range(100000):
+    for ch in s[:len(s) // 2]:
+        print(ch, end='')
+print(datetime.now() - t0)
+# 00:01.920824
+
+# t0 = datetime.now()
+# for _ in range(100000):
+#     for i in range(len(s) // 2):
+#         print(s[i], end='')
+# print(datetime.now() - t0)
+# 0:00:02.074452
+
+# t0 = datetime.now()
+# for _ in range(100000):
+#     print(s[len(s) // 2:])
+# print(datetime.now() - t0)
+# 0:00:01.125987
+
+# t0 = datetime.now()
+# for _ in range(100000):
+#     print(first_part(s))
+# print(datetime.now() - t0)
+# # 0:00:01.873350
