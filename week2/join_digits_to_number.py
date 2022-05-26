0a90055dc842ab73d554681776717c275cfdb7b3
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-05-27 00:07:39 +0700

    work with methods 'split' and 'join'

--- week2/join_digits_to_number.py
+++ week2/join_digits_to_number.py
@@ -2,16 +2,30 @@
 # input = 1 0 0
 # out = 1056
 
-first_string = input('Input digits in the list: ')
-second_string = input('Input digits in the other list: ')
-array1 = first_string.split()
-array2 = second_string.split()
+# first_string = input('Input digits in the list: ')
+# second_string = input('Input digits in the other list: ')
+# array1 = first_string.split()
+# array2 = second_string.split()
+#
+# first_string = ''.join(array1)
+# second_string = ''.join(array2)
+#
+# x1 = int(first_string)
+# x2 = int(second_string)
+#
+# print(x1 + x2)
 
-first_string = ''.join(array1)
-second_string = ''.join(array2)
 
-x1 = int(first_string)
-x2 = int(second_string)
 
-print(x1 + x2)
-
+print(
+    int(
+        ''.join(
+            input('Input digits in the list: ').split()
+        )
+    )
+    + int(
+        ''.join(
+            input('Input digits in the other list: ').split()
+        )
+    )
+)
