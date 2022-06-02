622502b51ff6b96c5a3b41a0af49e942cc9e56e9
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-06-02 15:01:22 +0700

    work with range and loops

---
+++ week2/using functions and loops.py
@@ -0,0 +1,19 @@
+# making list
+numbers = list(range(53))
+print(numbers)
+
+# counting the number of characters in a string
+str1 = "I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living."
+numbs = 0
+
+for w in str1:
+    numbs += 1
+print(numbs)
+
+# the sum of the numbers in the list
+numbers = list(range(41))
+sum1 = 0
+
+for w in numbers:
+    sum1 += w
+print(sum1)
