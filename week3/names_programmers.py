b49cee9ed88c341c884f3388066c129e48ed20c6
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-06-17 18:16:58 +0700

    number-dependent word endings

--- week3/names_programmers.py
+++ week3/names_programmers.py
@@ -6,13 +6,15 @@ assert int(count_progger) >= 0, 'Ошибка! Введите неотрицат
 last_one = count_progger[-1]
 
 if sym_count >= 2 and count_progger[-2:] in ('11', '12', '13', '14'):
-    print(count_progger, "программистов")
+    end_word = 'ов'
 
 elif last_one == '1':
-    print(count_progger, "программист")
+    end_word = ''
 
 elif last_one in ('2', '3', '4'):
-    print(count_progger, "программиста")
+    end_word = 'a'
 
 elif last_one in ('5', '6', '7', '8', '9', '0'):
-    print(count_progger, "программистов")
+    end_word = 'ов'
+
+print(count_progger, "программист" + end_word)
