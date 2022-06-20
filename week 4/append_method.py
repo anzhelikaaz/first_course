d47aa827ef8ff78036192c9cb89d01f2b75f93c7
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-06-20 19:48:02 +0700

    simple example of using append method

---
+++ week 4/append_method.py
@@ -0,0 +1,9 @@
+# Append method
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'Melbourne']
+trav_dest.append('Guadalajara')
+print(trav_dest)
+
+# Concatenation
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'Melbourne']
+trav_dest = trav_dest + ['Guadalajara']
+print(trav_dest)
