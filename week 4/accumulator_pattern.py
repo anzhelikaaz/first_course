60bcc15ceab323185dc582b13a2e02ebd42b1ba4
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-06-21 15:13:36 +0700

    accumulator pattern with list

---
+++ week 4/accumulator_pattern.py
@@ -0,0 +1,8 @@
+# Given the list of numbers, numbs, create a new list of those same numbers increased by 5.
+# Save this new list to the variable newlist.
+numbs = [5, 10, 15, 20, 25]
+newlist = []
+
+for num in numbs:
+    newlist.append(num + 5)
+print(newlist)
