4da983f86bd7aa6216387b6a1e8869c3eacc7823
anzhelikaaz <koshkarova.anzhelika@mail.ru>
2022-06-21 15:52:15 +0700

    multiply list elements by 2

--- week 4/accumulator_pattern.py
+++ week 4/accumulator_pattern.py
@@ -6,3 +6,11 @@ newlist = []
 for num in numbs:
     newlist.append(num + 5)
 print(newlist)
+
+# Multiply list elements by 2
+lst_nums = [4, 29, 5.3, 10, 2, 1817, 1967, 9, 31.32]
+larger_nums = []
+
+for num in lst_nums:
+    larger_nums.append(num * 2)
+print(larger_nums)
